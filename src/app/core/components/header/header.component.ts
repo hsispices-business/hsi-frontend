d944fe0707a9eb8b00846395f245da3067f82588
Abhinav Chaudhary <acrocks1000@gmail.com>
2026-04-13 23:58:15 +0530

    Minor updates

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Component, HostListener, ElementRef } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,6 +9,15 @@ import { RouterLink, RouterLinkActive } from "@angular/router";
 })
 export class HeaderComponent {
   mobileMenuOpen = false;
+
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.mobileMenuOpen && !this.elRef.nativeElement.contains(event.target)) {
+      this.mobileMenuOpen = false;
+    }
+  }
 
   selectedTab: string = 'products';
 
