9ed7a152f0bc5e6063aaca202986c3b86c6c200c
Abhinav Chaudhary <acrocks1000@gmail.com>
2026-05-25 23:48:14 +0530

    feat(hero): enhance hero section styling and animations with new text shine effect

--- src/app/core/services/base.service.ts
+++
@@ -1,9 +0,0 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpService } from './http.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class BaseService {
-  readonly httpService = inject(HttpService);
-}
